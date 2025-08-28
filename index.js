edadea60d36705ab9fc3edad74156c8a3800ed05
Mahfuz Shakib <mahfuzshakib301@gmail.com>
2025-08-28 22:53:26 +0600

    more ui polishing updated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const love = document.getElementsByClassName("heart-icon");
+const serviceTitle = document.getElementsByClassName("service-title");
 const service = document.getElementsByClassName("service");
 const helpline = document.getElementsByClassName("helpline");
 const copy = document.getElementsByClassName("copy");
@@ -29,9 +30,9 @@ function toggle_mode_for_mobile_device(id) {
 
 function toggle_button_bg(id)
 {
-  document.getElementById("see-hotlines-btn").classList.remove('bg-lime-100');
-  document.getElementById("see-call-history-btn").classList.remove('bg-lime-100');
-  document.getElementById(id).classList.add('bg-lime-100');
+  document.getElementById("see-hotlines-btn").classList.add('bg-lime-100');
+  document.getElementById("see-call-history-btn").classList.add('bg-lime-100');
+  document.getElementById(id).classList.remove('bg-lime-100');
 }
 
 //clicking on heart-icon of any cards
@@ -45,17 +46,17 @@ for (let i = 0; i < call.length; i++) {
   call[i].addEventListener("click", () => {
     const currentCoin = getValue("coin-count");
     if (currentCoin < 20) {
-      alert("You have not enoughed coin. To call atleast 20 coins needed.");
+      alert(`❌ আপনার কাছে পর্যাপ্ত কয়েন নেই। কল করতে কমপক্ষে 20 টি কয়েন লাগবে।`);
       return;
     }
     setValue("coin-count", currentCoin - 20);
 
-    alert(`Calling ${service[i].innerText} ${helpline[i].innerText}...`);
+    alert(`📞 Calling ${service[i].innerText} (${serviceTitle[i].innerText}) ${helpline[i].innerText} . . .`);
     const lastCall = document.createElement("div");
     lastCall.innerHTML = `
         <div class="bg-gray-50 flex justify-between items-center p-3 inter-font text-sm rounded-lg gap-1 mt-3 ">
             <div>
-                <h1 class="mb-[2px] font-semibold">${service[i].innerText}</h1>
+                <h1 class="mb-[2px] font-semibold">${serviceTitle[i].innerText}</h1>
                 <p class="text-gray-600">${helpline[i].innerText}</p>
             </div>
             <p>${new Date().toLocaleTimeString()}</p>
@@ -73,6 +74,7 @@ document.getElementById("clear-btn").addEventListener("click", () => {
 for (let i = 0; i < copy.length; i++) {
   copy[i].addEventListener("click", () => {
     setValue("copy-count", getValue("copy-count") + 1);
+    
     helpline[i].addEventListener("copy", () => {
       alert(`${helpline[i].innerText} number copied`);
     });
